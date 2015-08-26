144d8e81ed71351635dc17c5711c943a4413c2d6
nirmalasingam <nirmalasingam@outlook.fr>
2015-08-26 10:25:42 +0200

    blades update

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -1,4 +1,14 @@
+<?php
+//{{var_dump(Auth::user())}}
+?>
 @extends('logged_header')
 
-espace membre
+<div class="col-md-12">
+    <h1>Vos évènements inscrits disponibles</h1>
+</div>
+
+<div class="col-md-12">
+    <h1>Nos 3 prochains évènements</h1>
+</div>
+
 @extends('footer')
